2126cc2775d90fbc0579d0548c08b35e199632f7
tylerguy <tylerdenman200@gmail.com>
2024-04-23 08:52:56 +0100

    add integration to optimise images on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import remarkEleventyImage from "astro-remark-eleventy-image";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Kubrixss.github.io',
+  site: 'https://Kubrixss.github.io',
+  integrations: [remarkEleventyImage()],
+  vite: {
+    ssr: {
+      external: ["@11ty/eleventy-img"],
+    },
+}
+  
 });
